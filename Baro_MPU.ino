edce2d66ec008bc0899d7588cd599fb9bf12f5ba
Heisenberg <jainakshay0905@gmail.com>
2019-04-12 22:58:53 +0530

    Add files via upload

---
+++ Baro_MPU.ino
@@ -0,0 +1,345 @@
+
+
+#include "I2Cdev.h"
+#include <Adafruit_BMP085.h>
+#include "U8glib.h"
+
+#include "MPU6050_6Axis_MotionApps20.h"
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+
+    #include "Wire.h"
+#endif
+
+MPU6050 mpu;
+#define OUTPUT_READABLE_YAWPITCHROLL
+
+#define OUTPUT_TEAPOT
+
+Adafruit_BMP085 bmp;
+
+U8GLIB_SSD1306_128X64 u8g(U8G_I2C_OPT_NO_ACK); 
+
+float pressure = 0.0;
+float tempC = 0.0;
+float altitude = 0.0;
+void BmpSensorRead(float* pressure, float* tempC, float* altitude);
+void DisplayPresTemp(float* pressure, float* tempC, float* altitude);
+
+
+
+#define INTERRUPT_PIN 2 
+#define LED_PIN 13 
+bool blinkState = false;
+
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+
+uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+void setup() {
+    
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        Wire.setClock(400000); 
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    Serial.begin(115200);
+    while (!Serial); 
+    
+    Serial.println(F("Initializing I2C devices..."));
+    mpu.initialize();
+    pinMode(INTERRUPT_PIN, INPUT);
+
+
+    Serial.println(F("Testing device connections..."));
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+    
+    Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+    while (Serial.available() && Serial.read()); // empty buffer
+    while (!Serial.available());                 // wait for data
+    while (Serial.available() && Serial.read()); // empty buffer again
+
+    Serial.println(F("Initializing DMP..."));
+    devStatus = mpu.dmpInitialize();
+
+    mpu.setXGyroOffset(220);
+    mpu.setYGyroOffset(76);
+    mpu.setZGyroOffset(-85);
+    mpu.setZAccelOffset(1788); 
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
+    }
+
+    // configure LED for output
+    pinMode(LED_PIN, OUTPUT);
+
+    // assign default color value
+  if (u8g.getMode() == U8G_MODE_R3G3B2)
+  {
+    u8g.setColorIndex(255);     // white
+  }
+  else if (u8g.getMode() == U8G_MODE_GRAY2BIT)
+  {
+    u8g.setColorIndex(3);         // max intensity
+  }
+  else if (u8g.getMode() == U8G_MODE_BW)
+  {
+    u8g.setColorIndex(1);         // pixel on
+  }
+  else if (u8g.getMode() == U8G_MODE_HICOLOR)
+  {
+    u8g.setHiColorByRGB(255, 255, 255);
+  }
+
+  for (int a = 0; a < 30; a++)
+  {
+    u8g.firstPage();
+
+    do
+    {
+      u8g.setFont(u8g_font_fub11);
+      u8g.setFontRefHeightExtendedText();
+      u8g.setDefaultForegroundColor();
+      u8g.setFontPosTop();
+      u8g.drawStr(4, a, "BMP180 Sensor");
+    }
+    while (u8g.nextPage());
+  }
+
+  delay(3000);
+
+  if (!bmp.begin())
+  {
+    u8g.firstPage();
+
+    do
+    {
+      u8g.setFont(u8g_font_fub11);
+      u8g.setFontRefHeightExtendedText();
+      u8g.setDefaultForegroundColor();
+      u8g.setFontPosTop();
+      u8g.drawStr(4, 0, "BMP085 Sensor");
+      u8g.drawStr(4, 20, " ERROR!");
+    }
+    while (u8g.nextPage());
+
+    Serial.println("BMP085 sensor, ERROR!");
+
+    while (1) {}
+  }
+}
+
+
+void loop() {
+    // if programming failed, don't try to do anything
+    if (!dmpReady) return;
+
+    // wait for MPU interrupt or extra packet(s) available
+    while (!mpuInterrupt && fifoCount < packetSize) {
+        
+    }
+
+    // reset interrupt flag and get INT_STATUS byte
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+
+    // get current FIFO count
+    fifoCount = mpu.getFIFOCount();
+
+    // check for overflow (this should never happen unless our code is too inefficient)
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        // reset so we can continue cleanly
+        mpu.resetFIFO();
+        Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+    } else if (mpuIntStatus & 0x02) {
+        // wait for correct available data length, should be a VERY short wait
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+        // read a packet from FIFO
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        
+        // track FIFO count here in case there is > 1 packet available
+        // (this lets us immediately read more without waiting for an interrupt)
+        fifoCount -= packetSize;
+
+        #ifdef OUTPUT_READABLE_QUATERNION
+            // display quaternion values in easy matrix form: w x y z
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            Serial.print("quat\t");
+            Serial.print(q.w);
+            Serial.print("\t");
+            Serial.print(q.x);
+            Serial.print("\t");
+            Serial.print(q.y);
+            Serial.print("\t");
+            Serial.println(q.z);
+        #endif
+
+        #ifdef OUTPUT_READABLE_EULER
+            // display Euler angles in degrees
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetEuler(euler, &q);
+            Serial.print("euler\t");
+            Serial.print(euler[0] * 180/M_PI);
+            Serial.print("\t");
+            Serial.print(euler[1] * 180/M_PI);
+            Serial.print("\t");
+            Serial.println(euler[2] * 180/M_PI);
+        #endif
+
+        #ifdef OUTPUT_READABLE_YAWPITCHROLL
+            // display Euler angles in degrees
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+            Serial.print("ypr\t");
+            Serial.print(ypr[0] * 180/M_PI);
+            Serial.print("\t");
+            Serial.print(ypr[1] * 180/M_PI);
+            Serial.print("\t");
+            Serial.println(ypr[2] * 180/M_PI);
+        #endif
+
+        #ifdef OUTPUT_READABLE_REALACCEL
+            // display real acceleration, adjusted to remove gravity
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetAccel(&aa, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+            Serial.print("areal\t");
+            Serial.print(aaReal.x);
+            Serial.print("\t");
+            Serial.print(aaReal.y);
+            Serial.print("\t");
+            Serial.println(aaReal.z);
+        #endif
+
+        #ifdef OUTPUT_READABLE_WORLDACCEL
+            // display initial world-frame acceleration, adjusted to remove gravity
+            // and rotated based on known orientation from quaternion
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetAccel(&aa, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+            mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+            Serial.print("aworld\t");
+            Serial.print(aaWorld.x);
+            Serial.print("\t");
+            Serial.print(aaWorld.y);
+            Serial.print("\t");
+            Serial.println(aaWorld.z);
+        #endif
+    
+        #ifdef OUTPUT_TEAPOT
+            // display quaternion values in InvenSense Teapot demo format:
+            teapotPacket[2] = fifoBuffer[0];
+            teapotPacket[3] = fifoBuffer[1];
+            teapotPacket[4] = fifoBuffer[4];
+            teapotPacket[5] = fifoBuffer[5];
+            teapotPacket[6] = fifoBuffer[8];
+            teapotPacket[7] = fifoBuffer[9];
+            teapotPacket[8] = fifoBuffer[12];
+            teapotPacket[9] = fifoBuffer[13];
+            Serial.write(teapotPacket, 14);
+            teapotPacket[11]++; // packetCount, loops at 0xFF on purpose
+        #endif
+
+        // blink LED to indicate activity
+        blinkState = !blinkState;
+        digitalWrite(LED_PIN, blinkState);
+
+        BmpSensorRead(&pressure, &tempC, &altitude);
+  DisplayPresTemp(&pressure, &tempC, &altitude);
+  delay(1000);
+    }
+        
+}
+
+void DisplayPresTemp(float* pressure, float* tempC, float* altitude)
+{
+  u8g.firstPage();
+
+  do
+  {
+    u8g.setFont(u8g_font_fub11);
+    u8g.setFontRefHeightExtendedText();
+    u8g.setDefaultForegroundColor();
+    u8g.setFontPosTop();
+    u8g.drawStr(2, 0, "Pressure");
+    u8g.setPrintPos(75, 0);
+    u8g.print(*pressure);
+    u8g.drawStr(4, 20, "Temp C");
+    u8g.setPrintPos(75, 20);
+    u8g.print(*tempC);
+    u8g.drawStr(4, 40, "Altitude");
+    u8g.setPrintPos(75, 40);
+    u8g.print(*altitude);
+  }
+  while (u8g.nextPage());
+}
+void BmpSensorRead(float* pressure, float* tempC, float* altitude)
+{
+  *tempC = bmp.readTemperature();
+  Serial.print("Temperature = ");
+  Serial.print(*tempC);
+  Serial.println(" *C");
+
+  *pressure = bmp.readPressure() / 100.0;
+  Serial.print("Pressure = ");
+  Serial.print(*pressure / 100.0);
+  Serial.println(" hPa");
+
+  // Calculate altitude assuming 'standard' barometric
+  // pressure of 1013.25 millibar = 101325 Pascal
+  *altitude = bmp.readAltitude();
+  Serial.print("Altitude = ");
+  Serial.print(*altitude);
+  Serial.println(" meters");
+}
